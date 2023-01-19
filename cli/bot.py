bd58e9f3445bc41f906b314230a2971d6ff53d09
Becca Elenzil <beccaelenzil@gmail.com>
2023-01-19 13:21:23 -0800

    create naive bot

---
+++ cli/bot.py
@@ -0,0 +1,45 @@
+from game_functions import *
+from collections import Counter
+import copy
+level = "easy"
+
+
+def initialize_guesses(level):
+    level_info = get_level_info()
+    initial_guesses = []
+    for number in range(level_info[level]
+                        ["min"], level_info[level]["max"]+1):
+        initial_guesses.append(str(number)*level_info[level]["num"])
+
+    return initial_guesses
+
+
+def initialize_number_freq(level):
+    level_info = get_level_info()
+    number_freq = {}
+    for number in range(level_info[level]
+                        ["min"], level_info[level]["max"]+1):
+        number_freq[number] = level_info[level]["num"]
+    return number_freq
+
+
+def update_number_freq(guess, cor_nums, number_freq):
+    number_freq[guess[0]] = cor_nums
+    return number_freq
+
+
+def generate_code(level, number_freq):
+    level_info = get_level_info()
+    code = ""
+    count = 0
+    numbers = []
+    number_freq_copy = copy.copy(number_freq)
+
+    while count < level_info[level]["num"]:
+        number = str(random.choice(list(number_freq_copy.keys())))
+        if number_freq_copy[number] > 0:
+            code += number
+            number_freq_copy[number] -= 1
+            count += 1
+
+    return code
---
+++ cli/bot_main.py
@@ -0,0 +1,73 @@
+from game_functions import *
+from bot import *
+import requests
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+url = os.environ.get("API_URL")
+
+print_intro()
+[level, MAX_TRIES] = get_and_print_level()
+print("Let's play!")
+play = "Y"
+user_id = None
+initial_guesses = initialize_guesses(level)
+play_num = 0
+number_freq = {}
+while play == "Y":
+    [turn, code, guess, guesses, game_id, status_code] = initialize()
+
+    while code != guess and turn < MAX_TRIES:
+        while status_code != 201:
+            if play_num < len(initial_guesses):
+                guess = initial_guesses[play_num]
+            else:
+                guess = generate_code(level, number_freq)
+            response = requests.post(f"{url}plays/",
+                                     json={"code": guess, "level": level, "game_id": game_id, "user_id": user_id})
+            status_code = response.status_code
+            if status_code != 201:
+                response_body = response.json()
+                print(response_body["error"])
+
+        response_body = response.json()
+        guesses.append([guess, response_body["correct_nums"],
+                       response_body["correct_pos"]])
+        code = response_body["answer"]
+        if play_num < len(initial_guesses):
+            number_freq = update_number_freq(
+                guess, response_body["correct_nums"], number_freq)
+
+        play_num += 1
+
+        # get game info on first play
+        if not game_id:
+            game_id = response_body["game_id"]
+
+        turn += 1
+        if response_body["win"]:
+            print_play_info(guesses)
+            print("You guessed it!")
+            print("The code was... ", code)
+            initial_guesses = initialize_guesses(level)
+            play_num = 0
+            [play, game_id] = play_again()
+        elif turn == MAX_TRIES:
+            print_play_info(guesses)
+            print("You ran out of guesses.")
+            print("The code was... ", code)
+            initial_guesses = initialize_guesses(level)
+            play_num = 0
+            [play, game_id] = play_again()
+        else:
+            guess = "XXXX"
+            status_code = 400
+
+        if user_id and play != "Y":
+            response = requests.get(f"{url}users/{user_id}")
+            response_body = response.json()
+
+            pp.pprint(response_body["performance summary"])
+
+
+print("Thanks for playing!")
